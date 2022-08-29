aab323b6adffebce3b3f74027ada407fb27a902e
krish-vijayan <krishvijayan55@gmail.com>
2022-08-29 16:05:54 -0400

    Added player headshot api

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # NBA On Top
 
 This project is going to display NBA leaders for certain seasons, as well as other NBA statistics.
+
+Player Stats API: https://www.balldontlie.io/
+Player Picture API: https://github.com/kshvmdn/nba.js/blob/master/docs/api/DATA.md/
--- src/components/InputValues.js
+++ src/components/InputValues.js
@@ -1,23 +1,43 @@
-import React, { useState } from "react";
-import PlayerItem from "./PlayerItem";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-
-function FindPlayer(props) {
-  return <h1></h1>;
-}
+import { useOnKeyPress } from "./onKeyPress";
+import "../App.css";
 
 function InputValues() {
-  const [player, setPlayer] = useState();
-  const [team, setTeam] = useState();
+  const [player, setPlayer] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [pic, setPic] = useState("");
+  const [id, setId] = useState("");
+  const [season, setSeason] = useState("");
+  const [team, setTeam] = useState("");
+  const [games, setGames] = useState("");
+  const [pts, setPts] = useState("");
+  const [asts, setAsts] = useState("");
+  const [rebs, setRebs] = useState("");
+  const [invalid, setInvalid] = useState("");
+  const [newId, setNewId] = useState("");
+
+  var currentTime = new Date();
+  var year = currentTime.getFullYear();
 
   const handleSearch = (val) => {
     setPlayer(val.target.value);
   };
 
-  const apiHandler = () => {
+  const getPlayerHandler = () => {
     if (!player) {
       setTeam("Please enter a valid name");
+      setPic(null);
+      setFirstName(null);
+      setLastName(null);
+      setSeason(null);
+      setGames(null);
+      setPts(null);
+      setAsts(null);
+      setRebs(null);
     } else {
+      //API call for player description
       axios
         .get(`https://www.balldontlie.io/api/v1/players?search=${player}`)
         .then((res) => {
@@ -28,18 +48,74 @@ function InputValues() {
               player.toLocaleLowerCase()
             ) {
               setTeam(val.team.full_name);
+              setId(val.id);
+              setNewId(7);
+              setFirstName(val.first_name);
+              setLastName(val.last_name);
             }
-            console.log(team);
           });
         })
         .catch((e) => console.log(e));
     }
   };
+
+  //API call for player headshot (is only called when player newId updates!)
+  useEffect(() => {
+    axios
+      .get(
+        `https://www.balldontlie.io/api/v1/season_averages?player_ids[]=${id}`
+      )
+      .then((res) => {
+        console.log(res.data.data);
+
+        res.data.data.map((val, key) => {
+          setInvalid(null);
+          setSeason(`Season: ${val.season}-${val.season + 1}`);
+          setGames(`Number of Games Played: ${val.games_played} / 82  `);
+          setPts(`Points Per Game: ${val.pts}`);
+          setAsts(`Assists Per Game: ${val.ast}`);
+          setRebs(`Rebounds Per Game: ${val.reb}`);
+        });
+      })
+      .catch((e) => console.log(e));
+  }, [id]);
+
+  const getHeadshot = () => {
+    axios
+      .get(`https://data.nba.net/data/10s/prod/v1/${year}/players.json`)
+      .then((res) => {
+        console.log(res.data.league.standard);
+        res.data.league.standard.map((val, key) => {
+          if (firstName === val.firstName && lastName === val.lastName) {
+            setNewId(val.personId);
+          }
+        });
+      })
+      .catch((e) => console.log(e));
+
+    setPic(`https://cdn.nba.com/headshots/nba/latest/1040x760/${newId}.png`);
+  };
+  useEffect(() => {
+    getHeadshot();
+  }, [newId]);
+  useOnKeyPress(getPlayerHandler, "Enter");
+
   return (
     <div>
       <input onChange={handleSearch} placeholder="Enter player name"></input>
-      <button onClick={apiHandler}>Search</button>
+      <button onClick={getPlayerHandler}>Search</button>
+      <h1>{invalid}</h1>
+      <h1>
+        {firstName} {lastName}
+      </h1>
+      <div></div>
+      <img className="headshot" src={pic}></img>
       <h1>{team}</h1>
+      <h2>{season}</h2>
+      <h3>{games}</h3>
+      <h3>{pts}</h3>
+      <h3>{asts}</h3>
+      <h3>{rebs}</h3>
     </div>
   );
 }
