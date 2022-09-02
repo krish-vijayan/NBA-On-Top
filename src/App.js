990dd9005113b2547657598573c0a554b392f252
krish-vijayan <krishvijayan55@gmail.com>
2022-09-02 01:37:26 -0400

    Updated favico & title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import { useState } from "react";
 import InputValues from "./components/InputValues";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>NBA On Top!</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
       <InputValues />
     </div>
   );
